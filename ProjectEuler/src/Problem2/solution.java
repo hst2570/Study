608d6c957a199f56249e28f6be59ebe775f5832d
hst2570 <hst2570@gmail.com>
2016-01-30 13:54:31 +0900

    complete Problem2

--- ProjectEuler/src/Problem2/solution.java
+++ ProjectEuler/src/Problem2/solution.java
@@ -4,5 +4,34 @@
 package Problem2;
 
 public class solution {
+	private long 기준값;
 	
+	public solution(long 기준값) {
+		this.기준값 = 기준값; 
+	}
+
+	public long result(){
+		
+		long 이전값 = 1;
+		long temp;
+		long sum = 0;
+		
+		for(long i = 1; i<기준값 ;){
+			
+			temp = i;
+			i = 이전값 + i;
+			이전값 = temp;
+			
+			if ( i%2 == 0){
+				sum = sum + i;
+			}
+		}
+		
+		return sum;
+	}
+	
+	public static void main(String[] arg){
+		solution solution = new solution(4000000);
+		System.out.println(solution.result());
+	}
 }
