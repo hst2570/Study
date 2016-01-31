5c0cafefffb02382e3a0cb6acb038eb2fc384c3c
hst2570 <hst2570@gmail.com>
2016-01-31 23:41:07 +0900

    complete Euler Problem3

--- ProjectEuler/src/Problem3/solution.java
+++ ProjectEuler/src/Problem3/solution.java
@@ -1,8 +1,39 @@
 /*
- * 짝수이면서 4백만 이하인 모든 항을 더하면 얼마가 됩니까?
+ * 600851475143의 소인수 중에서 가장 큰 수를 구하세요.
  */
 package Problem3;
 
 public class solution {
+	private long 기준값;
 	
+	public solution(long 기준값) {
+		this.기준값 = 기준값; 
+	}
+
+	public long result(){
+		
+		long 나눈값 = 기준값;
+		int i = 2;
+		
+		while(i < 나눈값){
+			if(나눈값%i == 0){
+				for(int j = 2; j<i; j++){
+					if(i%j == 0){
+						break;
+					}else if(i%j != 0 && j == i-1){
+						나눈값 = 나눈값/i;
+					}
+				}
+			}else{
+				i++;
+			}
+		}
+		
+		return 나눈값;
+	}
+	
+	public static void main(String[] arg){
+		solution solution = new solution(600851475143L);
+		System.out.println(solution.result());
+	}
 }
