3730789227943765f975c854a3dcc6bda4173a8c
hst2570 <hst2570@gmail.com>
2016-02-01 23:15:28 +0900

    update Euler Problem4 README

--- ProjectEuler/src/Problem4/solution.java
+++ ProjectEuler/src/Problem4/solution.java
@@ -14,27 +14,10 @@ public class solution {
 		this.기준값 = 기준값; 
 	}
 
-	public long result(){
-		
-		long 나눈값 = 기준값;
-		int i = 2;
-		
-		while(i < 나눈값){
-			if(나눈값%i == 0){
-				for(int j = 2; j<i; j++){
-					if(i%j == 0){
-						break;
-					}else if(i%j != 0 && j == i-1){
-						나눈값 = 나눈값/i;
-					}
-				}
-			}else{
-				i++;
-			}
-		}
-		
-		return 나눈값;
-	}
+//	public long result(){
+//		
+//		
+//	}
 	
 	public static void main(String[] arg){
 		solution solution = new solution(600851475143L);
