b979cb4d1daa03d019a4e735be5dcbaef84dfaa9
hst2570 <hst2570@gmail.com>
2016-02-03 21:31:36 +0900

    add Euler Problem5

---
+++ ProjectEuler/src/Problem5/solution.java
@@ -0,0 +1,26 @@
+/*
+ * 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수
+ */
+package Problem5;
+
+public class solution {
+//	private long 기준값;
+	
+//	public solution(long 기준값) {
+//		this.기준값 = 기준값; 
+//	}
+	
+	public static String reverse(String source){
+	       
+	}
+
+	public int result(){
+		
+		
+	}
+	
+	public static void main(String[] arg){
+		solution solution = new solution();
+		System.out.println(solution.result());
+	}
+}
