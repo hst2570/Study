34710b95750b9b4e89aa1e2be0b24ca850944ead
hst2570 <hst2570@gmail.com>
2016-02-05 22:22:56 +0900

    create Euler Problem6

---
+++ ProjectEuler/src/Problem6/solution.java
@@ -0,0 +1,27 @@
+/*
+ * 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수
+ */
+package Problem6;
+
+public class solution {
+	private int 기준값;
+	
+	public solution(int 기준값) {
+		this.기준값 = 기준값; 
+	}
+	
+	public long result(){
+		long 제곱의합 = 0;
+		long 합의제곱 = 0;
+		long 차 = 0;
+		
+		//제곱의 합과 합의 제곱을 뺀다.
+		
+		return 차;
+	}
+	
+	public static void main(String[] arg){
+		solution solution = new solution(100);
+		System.out.println(solution.result());
+	}
+}
