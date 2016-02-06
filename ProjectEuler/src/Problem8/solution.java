2c0f045dd56a0ef4d81af9f5353dcbe8e231b1ba
hst2570 <hst2570@gmail.com>
2016-02-06 12:10:18 +0900

    create Euler Problem8

---
+++ ProjectEuler/src/Problem8/solution.java
@@ -0,0 +1,20 @@
+
+package Problem8;
+
+public class solution {
+	private int 기준값;
+	
+	public solution(int 기준값) {
+		this.기준값 = 기준값; 
+	}
+	
+	public long result(){
+
+			
+	}
+	
+	public static void main(String[] arg){
+		solution solution = new solution();
+		System.out.println(solution.result());
+	}
+}
