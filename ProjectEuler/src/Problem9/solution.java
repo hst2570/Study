36626c7c53a920c17ed2aa5b6760f6bbeac2ca06
hst2570 <hst2570@gmail.com>
2016-02-08 13:28:55 +0900

    create Euler Problem9

---
+++ ProjectEuler/src/Problem9/solution.java
@@ -0,0 +1,20 @@
+
+package Problem9;
+
+public class solution {
+	private String 기준값;
+	
+	public solution(String 기준값) {
+		this.기준값 = 기준값; 
+	}
+	
+	
+	public int result(){
+		
+	}
+	
+	public static void main(String[] arg){
+		solution solution = new solution();
+		System.out.println(solution.result());
+	}
+}
